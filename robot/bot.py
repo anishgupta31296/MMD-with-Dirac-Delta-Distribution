d9734eb191fdf1825869ff2ed3a1a0649487b14f
anishgupta31296 <anishgupta31296@gmail.com>
2021-10-29 11:12:44 +0530

    Update bot.py

--- robot/bot.py
+++ robot/bot.py
@@ -125,7 +125,7 @@ class NonHolonomicBot(Agent):
         self.set_velocity(velocity)
         self.update_noise()
   
-    def sample_controls(self,samples=22):
+    def sample_controls(self,samples=25):
         agent_v=np.array([self.get_linear_velocity(),self.get_angular_velocity()])
         bounds=self.get_bounds()
         v_ctr_bounds=self.get_linear_velocity_control_bounds()
